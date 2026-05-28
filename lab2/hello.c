db2df8ee50e1d3c8fd169fefe6d0750c20218a25
aikkku <aismatil@u.rochester.edu>
2026-05-28 13:57:08 -0400

    lab1 comleted, lab2 ongoing

--- lab2/hello.c
+++ lab2/hello.c
@@ -5,9 +5,42 @@
 char _license[] SEC("license") = "GPL";
 
 // Step 1: define an eBPF map here
+#define MAX_PATH 256
+
+struct path_key {
+    char path[MAX_PATH];
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 16384);
+    __type(key, struct path_key);
+    __type(value, __u64);
+} exec_count SEC(".maps");
+
+
 
 SEC("tracepoint/syscalls/sys_enter_execve")
 int handle_execve_tp(struct trace_event_raw_sys_enter *ctx) {
     // Step 2: Add map logic code here
+    const char *filename = (const char *)ctx->args[0];
+
+    struct path_key key = {};
+    long n = bpf_probe_read_user_str(key.path, sizeof(key.path), filename);
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    __u64 *val = bpf_map_lookup_elem(&exec_count, &key);
+    if (val) {
+        *val += 1; //not safe to do so, but for now it's fine
+    } else {
+        __u64 init = 1;
+        bpf_map_update_elem(&exec_count, &key, &init, BPF_NOEXIST);
+    }
+
+    bpf_printk("execve: %s\n", key.path);
+
     return 0;
 }
